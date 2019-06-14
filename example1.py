4a16f95fe26d8dd35ddbbb6e83c141587d4609e7
Mahmut Bilgen <mbilgen@Mahmuts-iMac.local>
2019-06-14 07:46:53 -0700

    initial

---
+++ example1.py
@@ -0,0 +1,30 @@
+#!/usr/local/bin/python3.7
+import os
+
+class Node:
+    def __init__(self,data_value=None):
+        self.data_value=data_value
+        self.next_value=None
+
+class aLinkedList:
+    def __init__(self):
+        self.head_value=None
+
+    def print_list(self):
+        print_value = self.head_value
+        while print_value is not None:
+            print (print_value.data_value)
+            print_value = print_value.next_value
+
+
+if __name__=='__main__' :
+    alist = aLinkedList()
+    alist.head_value = Node("Mon")
+    e2 = Node("Tue") ; e3 = Node("Wed")
+
+    alist.head_value.next_value = e2
+
+    if e2.next_value is  None:
+        e2.next_value = e3
+
+    alist.print_list()
